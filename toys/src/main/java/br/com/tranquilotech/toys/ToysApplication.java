4a55206a299bf6aad0fed7d33da47918f60cc924
vitorvhsilva <dndragonbr@gmail.com>
2025-05-03 23:53:33 -0300

    Inicializando o repositório do projeto

---
+++ toys/src/main/java/br/com/tranquilotech/toys/ToysApplication.java
@@ -0,0 +1,13 @@
+package br.com.tranquilotech.toys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToysApplication.class, args);
+	}
+
+}
---
+++ toys/src/test/java/br/com/tranquilotech/toys/ToysApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tranquilotech.toys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
