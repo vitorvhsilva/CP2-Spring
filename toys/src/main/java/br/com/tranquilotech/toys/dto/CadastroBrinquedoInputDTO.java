5ba0bdcd50166aba601b17b6d7a1d24a408d7e44
vitorvhsilva <dndragonbr@gmail.com>
2025-05-04 00:06:34 -0300

    Aplicando Bean Validation no DTO de Cadastro do Brinquedo

--- toys/src/main/java/br/com/tranquilotech/toys/dto/CadastroBrinquedoInputDTO.java
+++ toys/src/main/java/br/com/tranquilotech/toys/dto/CadastroBrinquedoInputDTO.java
@@ -2,6 +2,7 @@ package br.com.tranquilotech.toys.dto;
 
 import br.com.tranquilotech.toys.domain.ClassificacaoBrinquedo;
 import br.com.tranquilotech.toys.domain.TipoBrinquedo;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -9,10 +10,14 @@ import java.math.BigDecimal;
 
 @Data
 public class CadastroBrinquedoInputDTO {
-    @Size()
+    @Size(min = 3, message = "O nome deve ter no mínimo 3 letras")
     private String nome;
+    @NotNull(message = "É necessário colocar o tipo do brinquedo")
     private TipoBrinquedo tipo;
+    @NotNull(message = "É necessário colocar a classificação do brinquedo")
     private ClassificacaoBrinquedo classificacao;
+    @NotNull(message = "É necessário colocar o tamanho do brinquedo")
     private Double tamanho;
+    @NotNull(message = "É necessário colocar o preço do brinquedo")
     private BigDecimal preco;
 }
